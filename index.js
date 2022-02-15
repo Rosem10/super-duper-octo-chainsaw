066b9a366cf6fce5556516b05ccf69fc122d6566
Rose Mullan <rfmullan@gmail.com>
2022-02-15 10:05:58 +0000

    add getFaveColour function

---
+++ index.js
@@ -0,0 +1,8 @@
+function getFaveColour(person){
+    const faveColours = {
+        "rose": "yellow"
+    }
+    console.log(`${person}'s favourite colour is ${faveColours[person]}`)
+}
+
+getFaveColour("rose")
